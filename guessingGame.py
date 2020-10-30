34fd9f75ba8fc6ae5dacc844e78d4a7d6e8431f7
kunalRaut21 <69686364+kunalRaut21@users.noreply.github.com>
2020-10-30 07:34:57 +0530

    Added One file
    guessing game in python

---
+++ guessingGame.py
@@ -0,0 +1,27 @@
+#python 3.7.1
+import random
+trials=0
+r=random.randrange(1,18)
+
+def left():
+  print("Only",9-(trials+1),"Attempt Left !! ")
+  
+
+while trials<9:
+  n=int(input("Guess the no:"))
+  if n<18:
+    if r==n:
+      print("Congratulations !! You Guess the No",r,"Correctly")
+      print("You Guessed the Number Correctly in",trials+1,"Attempt.")
+      break
+    elif n<r:
+      print("You guess the No Less than Magic No!!")
+      left()
+    elif n>r:
+      print("You Gussed no to High Than Magic No !!")
+      left()
+    else:
+      print("Game Over")
+  else:
+    print("Error! Tip:Enter No less than 19")
+  trials+=1
